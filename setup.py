cc5d4b34b20c96dace1450cebcec2cba60b673fd
Jakub Kadlčík <frostyx@email.cz>
2016-01-24 18:37:09 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name="frostyx.cz",
+    version='0.1',
+    description="My personal blog",
+    author="FrostyX",
+    author_email="frostyx@email.cz",
+    url="http://frostyx.cz",
+    install_requires=["flask"],
+)
